f356cb5a7ab06b709b33324fe7c693b22fed550c
Arthur <deathvango@gmail.com>
2017-07-31 20:09:49 -0500

    moved mongodb files to separate solution

--- src/mongodb/schema/ddl_createroles.js
+++
@@ -1,9 +0,0 @@
-db.createRole({
-  role: "api",
-  privileges: [
-     { resource: {db: "arartistry", collection: "" }, actions: [ "insert", "update", "remove" ] }
-  ],
-  roles: [
-     { role: "read", db: "arartistry" }
-  ]
-});
